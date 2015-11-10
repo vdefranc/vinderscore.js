5898bd941ee7d7737c903702bbae7a5fb7adc6b0
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-11-10 17:36:15 -0500

    fn first is working as expected

--- app/arrays/first.js
+++ app/arrays/first.js
@@ -2,10 +2,21 @@
  * [first description]
  * @param  {Array} array  
  * @param  {Number} n [The number of items to return from the begining of the array]
- * @return {Array}
+ * @return {Array || number}} If an n param is passed, function returns an array. Otherwise returns a number
  */
 
 vs.first = function (array, n) {
+  var result;
 
-  return array;
+  if (n) {
+    result = [];
+
+    for (var i = 0; i < n; i++) {
+      result.push(array[i]);
+    }
+  } else {
+    result = array.slice(0,1)[0];
+  }
+
+  return result;
 };
--- app/arrays/first.spec.js
+++ app/arrays/first.spec.js
@@ -1,10 +1,35 @@
 describe('vs.first', function () {
 
-  it('should return an array', function () {
-    var result = Array.isArray(vs.first([1,2])),
-        expected = true;
+  describe('if called with one argument', function () {
+    it('should return an number', function () {
+      var result = typeof vs.first([1]),
+          expected = 'number';
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+
+    it('should return the first number in the argument', function () {
+      var arg = [5,4,3,2,1],
+          result = vs.first(arg);
+
+      expect(result).toEqual(arg[0]);
+    });
   });
-  
+
+  describe('if called with two arguments', function () {
+    it('should return an array', function () {
+      var result = Array.isArray( vs.first([1,2], 2) ),
+          expected = true;
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should return the first n cells of the argument', function () {
+      var arg = [5,4,3,2,1],
+          result = vs.first(arg, 3),
+          expected = [5,4,3];
+
+      expect(result).toEqual(expected);
+    });
+  });
 });

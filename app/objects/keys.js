f041bd90aeb3c4599ca4bae104d5583fc2e306c8
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-12-17 05:42:11 -0500

    fn:keys is complete

---
+++ app/objects/keys.js
@@ -0,0 +1,25 @@
+/**
+ * vs.keys
+ *
+ * http://underscorejs.org/#keys
+ *
+ * Retrieve an array of the names of the object's own enumerable properties.
+ *
+ * @param { Object } obj - an object to be parsed
+ * @returns { Array } the array will contain the names of all the objects's keys
+ * 
+ */
+
+vs.keys = function (obj) {
+  var result = [],
+      prop;
+
+  for (prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      console.log(prop);
+      result.push(prop);
+    }
+  }
+
+  return result;
+};
---
+++ app/objects/keys.spec.js
@@ -0,0 +1,37 @@
+describe('vs.keys:', function () {
+
+  it('is compete', function () {
+    expect(true).toEqual(true);
+  });
+  
+  var propsToAdd, testObj = {}, result;
+
+  propsToAdd = [
+   'name', 'age' 
+  ];
+
+  propsToAdd.forEach(function(prop) {
+    testObj[prop] = 1;
+  });
+
+  result = vs.keys(testObj);
+
+
+  it('returns an array', function () {
+    var blankResult = vs.keys();
+    expect(Array.isArray( result )).toEqual(true);
+  });
+
+
+  describe('the returned array', function () {
+    it('contains all the test object\'s own properties', function () {
+      expect(propsToAdd).toEqual(result);
+    });
+  });
+
+
+
+
+  
+
+});
